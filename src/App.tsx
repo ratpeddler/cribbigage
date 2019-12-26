8d54e64d195555465033c324d67ddf282c4770f7
Alex Breyfogle <alexbr@microsoft.com>
2019-12-25 23:26:48 -0800

    Update score display section

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { deal, createDeck } from './game/deal';
 import { scoreHand } from './game/score';
 
 const players = 2; // Number of players
-const handSize = 4; // Dealt hand size to each player
+const handSize = 5; // Dealt hand size to each player
 const dealerExtra = 0; // For 3 hand
 const cribExtra = 0; // For CribBIGage
 const discardUntil = 4;
@@ -13,7 +13,7 @@ const discardUntil = 4;
 const App: React.FC = () => {
   const [scores] = React.useState([0, 0]);
   const { hands, crib, cut } = deal(createDeck(), players, handSize, dealerExtra, cribExtra);
-  
+
   const score = scoreHand(hands[0], cut);
 
   return (
@@ -21,16 +21,19 @@ const App: React.FC = () => {
       <ScoreBoard scores={scores} />
       Crib!
       <Hand cards={crib} />
-      
+
       Cut!
       <Hand cards={cut} />
-      
+
       {hands.map((cards, index) => index == 0 && <Hand cards={cards} key={index} maxKeep={discardUntil} />)}
 
-      <div>SCORE: {score.score}</div>
-      <div>fifteen: {score.fifteen}</div>
-      <div>pairs: {score.pairs}</div>
-      <div>knobs: {score.knobs}</div>
+      {/* Display the scores for now while finishing scoring */}
+      {!!score.score && <div>SCORE: {score.score}</div>}
+      {!!score.fifteen && <div>fifteen: {score.fifteen}</div>}
+      {!!score.pairs && <div>pairs: {score.pairs}</div>}
+      {!!score.knobs && <div>knobs: {score.knobs}</div>}
+      {!!score.runs && <div>runs: {score.runs}</div>}
+      {!!score.flush && <div>knobs: {score.flush}</div>}
     </div>
   );
 }
