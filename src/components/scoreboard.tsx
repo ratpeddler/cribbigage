9bee4bd1942c55bf141c74a9cf6576f30744fbd9
Alex Breyfogle <alexbr@microsoft.com>
2019-12-27 00:10:42 -0800

    Update scoreboard rendering

--- src/components/scoreboard.tsx
+++ src/components/scoreboard.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import { PlayerState } from "../game/turns";
 
 export const ScoreBoard: React.FC<{ players: PlayerState[] }> = props => {
-    return <div>
-        {props.players.map((p, i) => <Score score={p.score} player={p.name} key={i} hasCrib={i == props.players.length - 1} />)}
+    const maxScore = 120;
+
+    const colors = ["blue", "red", "green", "gold"];
+    const body: JSX.Element[] = [];
+    for (let i = 0; i < maxScore; i++) {
+        body.push(<div style={{ display: "flex", flexDirection: "column" }}>{props.players.map((p, pi) => <ScoreDot hasPlayer={p.score == i} playerColor={colors[pi]} />)}</div>);
+    }
+
+    return <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+        {body}
     </div>;
 }
 
-export const Score: React.FC<{ score: number, player: string, hasCrib?: boolean }> = props => {
-    return <div>
-        {`${props.player}: ${props.score}`}
-        {props.hasCrib ? " (CRIB)" : null}
-    </div>;
+const ScoreDot: React.FC<{ hasPlayer: boolean, playerColor: string }> = props => {
+    const diameter = 5;
+    return <div style={{ margin: 2, width: diameter, height: diameter, borderRadius: diameter, backgroundColor: props.hasPlayer ? props.playerColor : "grey" }}></div>;
 }
