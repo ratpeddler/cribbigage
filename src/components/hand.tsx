854c8c9bdb95c1b38cf101c6a2d7d2ab583b74ff
Alex Breyfogle <alexbr@microsoft.com>
2019-12-26 22:07:12 -0800

    Add card images

--- src/components/hand.tsx
+++ src/components/hand.tsx
@@ -1,45 +1,26 @@
 import React from "react";
-import { parseCard } from "../game/card";
 import { HandScore } from "./handScore";
+import { Card } from "./card";
 
 export const Hand: React.FC<{ cards: number[], maxKeep?: number, cut?: number[], keepCards: { [card: number]: boolean }, setKeepCards: (newValue: { [card: number]: boolean }) => void }> = props => {
 
-    return <div style={{ display: "flex", flexDirection: "row" }}>
-        Hand:
-        {props.cards.map((card, i) => <Card card={card} index={i} key={i} throw={!!props.maxKeep && !props.keepCards[i]} onClick={() => {
-            if (props.maxKeep) {
-                let newthrow = { ...props.keepCards };
-                const keys = Object.keys(newthrow).filter(key => !!newthrow[key as any]);
-                if (keys.length >= props.maxKeep && !props.keepCards[i]) {
-                    return;
+    return <>
+        <div key="hand" style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+            {props.cards.map((card, i) => <Card card={card} index={i} key={i} throw={!!props.maxKeep && !props.keepCards[i]} onClick={() => {
+                if (props.maxKeep) {
+                    let newthrow = { ...props.keepCards };
+                    const keys = Object.keys(newthrow).filter(key => !!newthrow[key as any]);
+                    if (keys.length >= props.maxKeep && !props.keepCards[i]) {
+                        return;
+                    }
+
+                    newthrow[i] = !newthrow[i];
+                    props.setKeepCards(newthrow);
                 }
+            }} />)}
 
-                newthrow[i] = !newthrow[i];
-                props.setKeepCards(newthrow);
-            }
-        }} />)}
-
-        {/* Show the score for the currently selected cards only */}
-        <HandScore hand={props.cards.filter((card, index) => props.keepCards[index])} cut={props.cut} />
-    </div>;
-}
-
-export const Card: React.FC<{ card: number, index: number, throw?: boolean, onClick?: () => void }> = props => {
-    const { value, suit } = parseCard(props.card);
-
-    return <div style={{
-        backgroundColor: props.throw ? "lightgrey" : undefined,
-        color: suit == "Diamonds" || suit == "Hearts" ? "red" : "black",
-        border: "1px solid black",
-        alignItems: "center",
-        height: "100px",
-        width: "75px",
-        margin: "15px",
-        padding: "5px",
-    }}
-        onClick={props.onClick}
-    >
-        <div>{value}</div>
-        <div>{suit}</div>
-    </div>;
+            {/* Show the score for the currently selected cards only */}
+        </div>
+        <HandScore key="score" hand={props.cards.filter((card, index) => props.keepCards[index])} cut={props.cut} />
+    </>;
 }
