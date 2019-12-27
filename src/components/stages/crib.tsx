e1e96298ba86c73b45f552726f77493891796fb2
Alex Breyfogle <alexbr@microsoft.com>
2019-12-27 06:31:29 -0800

    Add score from crib

--- src/components/stages/crib.tsx
+++ src/components/stages/crib.tsx
@@ -3,6 +3,7 @@ import { GameComponent } from "../game";
 import { HandScore } from "../handScore";
 import { Hand } from "../hand";
 import { Button } from "../button";
+import { scoreHand } from "../../game/score";
 
 export const Crib: GameComponent = props => {
     return <div>
@@ -13,6 +14,21 @@ export const Crib: GameComponent = props => {
         <Hand cards={props.game.crib!} />
         <HandScore hand={props.game.crib!} cut={props.game.cut} />
 
-        <Button onClick={() => props.setGameState(props.game, true)}>Next</Button>
+        <Button onClick={() => {
+            // last player is always dealer
+            let players = [...props.game.players];
+            let last = players.pop()!;
+            last = {
+                ...last,
+                score: last.score + scoreHand(props.game.crib!, props.game.cut!).score
+            };
+            
+            players.push(last);
+
+            props.setGameState({
+                ...props.game,
+                players
+            }, true);
+        }}>Next</Button>
     </div>;
 }
