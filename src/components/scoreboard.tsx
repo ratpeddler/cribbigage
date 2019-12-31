e0ff247f11d4342d34358b006b037ca155c16857
Alex Breyfogle <alexbr@microsoft.com>
2019-12-30 17:29:54 -0800

    Update board styles for clarity

--- src/components/scoreboard.tsx
+++ src/components/scoreboard.tsx
@@ -3,10 +3,12 @@ import { PlayerState } from "../game/turns";
 import arrow_left from "../arrow_left.png";
 import arrow_right from "../arrow_right.png";
 
+const boardColor = "sandybrown";
+
 const colors = ["blue", "red", "green", "gold"];
 const byPlayerName = (a: PlayerState, b: PlayerState) => {
     if (a.name > b.name) { return 1; }
-    if (a.name == b.name) { return 0; }
+    if (a.name === b.name) { return 0; }
     return -1;
 };
 
@@ -29,19 +31,29 @@ export const ScoreBoard: React.FC<{ players: PlayerState[] }> = props => {
 }
 
 const ScoreDot: React.FC<{ hasPlayer: boolean, playerColor: string, index: number }> = props => {
-    const diameter = 14;
-    return <div title={props.index.toString()} style={{ margin: 2, width: diameter, height: diameter, borderRadius: diameter, backgroundColor: props.hasPlayer ? props.playerColor : "grey" }}></div>;
+    const diameter = 10;
+    const border = 4;
+    return <div
+        title={props.index.toString()}
+        style={{
+            margin: 2,
+            width: diameter,
+            height: diameter,
+            borderRadius: diameter,
+            backgroundColor: props.hasPlayer ? props.playerColor : "grey",
+            border: `${border}px solid ${props.hasPlayer ? props.playerColor : boardColor}`,
+        }}></div>;
 }
 
 const Board: React.FC<{ players: PlayerState[], total: number, lines: number }> = props => {
     const { total, lines, players } = props;
     const perRow = Math.floor(total / lines);
-    if (total / lines != perRow) { throw "Bad choice of line numbers! doesn't divide evenly!" }
+    if (total / lines !== perRow) { throw "Bad choice of line numbers! doesn't divide evenly!" }
     const body: JSX.Element[] = [];
     for (let i = 0; i < lines; i++) {
-        body.push(<ScoreRow key={i} players={players} dots={perRow} from={perRow * i} reverse={i % 2 != 0} />)
+        body.push(<ScoreRow key={i} players={players} dots={perRow} from={perRow * i} reverse={i % 2 !== 0} />)
     }
-    return <div style={{ display: "flex", flexDirection: "column" }}>
+    return <div style={{ display: "flex", flexDirection: "column", backgroundColor: boardColor }}>
         {body}
     </div>
 }
@@ -52,8 +64,15 @@ const ScoreRow: React.FC<{ players: PlayerState[], dots: number, from: number, r
     const body: JSX.Element[] = [];
 
     for (let i = reverse ? dots + from - 1 : from; reverse ? i >= from : i < dots + from; reverse ? i-- : i++) {
-        body.push(<div key={i} style={{ display: "flex", flexDirection: "column", marginTop: 1, backgroundColor: i % 30 == 0 ? "orange" : i % 5 == 0 ? "lightgrey" : undefined }}>
-            {players.map((p, pi) => <ScoreDot key={pi} index={i} hasPlayer={p.score == i || p.lastScore == i} playerColor={colors[pi]} />)}
+        body.push(<div
+            key={i}
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                marginTop: 1,
+                outline: i % 30 === 0 ? "2px solid yellow" : i % 5 === 0 ? "1px solid black" : undefined
+            }}>
+            {players.map((p, pi) => <ScoreDot key={pi} index={i} hasPlayer={p.score === i || p.lastScore === i} playerColor={colors[pi]} />)}
         </div>);
     }
 
