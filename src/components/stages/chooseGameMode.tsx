ca9b19fe1397aafef3ebdf8e8f659c3786174e4f
Alex Breyfogle <alexbr@microsoft.com>
2019-12-30 17:39:48 -0800

    Update logo

--- src/components/stages/chooseGameMode.tsx
+++ src/components/stages/chooseGameMode.tsx
@@ -4,6 +4,7 @@ import { Button } from "../button";
 import { GameModes } from "../../game/rules";
 import { startGame } from "../../game/game";
 import { PlayerInfo } from "../../game/turns";
+import logo from "./../../cribbigage.png";
 
 // Total hack for now
 export const IsYou = (player: PlayerInfo): boolean => {
@@ -12,6 +13,7 @@ export const IsYou = (player: PlayerInfo): boolean => {
 
 export const ChooseGameMode: GameComponent = props => {
     return <div style={{ height: "100%", width: "100%", textAlign: "center" }}>
+        <img alt="CribBIGage!" src={logo} style={{ maxWidth: "70%", flex: "auto", maxHeight: "30%" }} />
         <h1>Choose a game mode</h1>
         {GameModes.map(mode => <div key={mode.name}>
             <Button
