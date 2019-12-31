281a747ad926942ea8d859b61d3f286a03fe2243
Alex Breyfogle <alexbr@microsoft.com>
2019-12-30 17:10:30 -0800

    Update crib for more clarity

--- src/components/stages/crib.tsx
+++ src/components/stages/crib.tsx
@@ -1,35 +1,42 @@
 import React from "react";
-import { GameComponent } from "../game";
+import { GameComponent, Game } from "../game";
 import { HandScore } from "../handScore";
 import { Hand } from "../hand";
 import { Button } from "../button";
 import { scoreHand } from "../../game/score";
 
 export const Crib: GameComponent = props => {
-    return <div>
-        Cut:
-        <Hand cards={props.game.cut!} />
+    return <div style={{ height: "100%", width: "100%", display: "flex", flexDirection: "row" }}>
+        <div style={{ flex: "none", padding: 15 }}>
+            Cut:
+            <Hand cards={props.game.cut!} />
+            <div>
+                <Button onClick={() => {
+                    // last player is always dealer
+                    let players = [...props.game.players];
+                    let last = players.pop()!;
+                    last = {
+                        ...last,
+                        score: last.score + scoreHand(props.game.crib!, props.game.cut!).score,
+                        lastScore: last.score // Strange coincidence :P
+                    };
 
-        Score of crib:
-        <Hand cards={props.game.crib!} />
-        <HandScore hand={props.game.crib!} cut={props.game.cut} />
+                    players.push(last);
+                    if (last.score >= 120) { alert(`${last.name} won!`) }
 
-        <Button onClick={() => {
-            // last player is always dealer
-            let players = [...props.game.players];
-            let last = players.pop()!;
-            last = {
-                ...last,
-                score: last.score + scoreHand(props.game.crib!, props.game.cut!).score,
-                lastScore: last.score // Strange coincidence :P
-            };
-            
-            players.push(last);
-
-            props.setGameState({
-                ...props.game,
-                players
-            }, true);
-        }}>Next</Button>
+                    props.setGameState({
+                        ...props.game,
+                        players
+                    }, true);
+                }}>Next</Button>
+            </div>
+        </div>
+        <div style={{ flex: "auto" }}>
+            <h3>Crib for {props.game.players[props.game.players.length - 1].name}:</h3>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <div><Hand cards={props.game.crib!} /></div>
+                <div><HandScore hand={props.game.crib!} cut={props.game.cut} /></div>
+            </div>
+        </div>
     </div>;
 }
