395512365ce66955aa131283563fdf7e9195f43a
macflash <alex.brofo@outlook.com>
2020-01-01 10:46:54 -0800

    Update deal

--- src/components/stages/deal.tsx
+++ src/components/stages/deal.tsx
@@ -2,16 +2,35 @@ import React from "react";
 import { GameComponent } from "../game";
 import { RunDeal } from "../../game/deal";
 import { Button } from "../button";
+import { IsYou } from "./chooseGameMode";
+import { getCurrentDealer } from "../../game/play";
 
 export const Deal: GameComponent = props => {
     const Layout = props.layout;
-    return <Layout game={props.game} userActions={() =>
-        <Button
-            big={true}
-            onClick={() => {
+    let dealer = getCurrentDealer(props.game);
+    let yourCrib = IsYou(dealer);
+
+    React.useEffect(() => {
+        if (!yourCrib) {
+            setTimeout(() => {
                 props.setGameState(RunDeal(props.game), true);
-            }}
-        >
-            Deal
-        </Button>} />;
+            }, 1000);
+        }
+    }, []);
+
+    // TODO: Animate dealing
+
+    return <Layout game={props.game} userActions={() =>
+        <>
+            {yourCrib && <h3>It's your turn to deal</h3>}
+            <Button
+                disabled={!yourCrib}
+                loading={!yourCrib}
+                big={true}
+                onClick={() => {
+                    props.setGameState(RunDeal(props.game), true);
+                }}
+            >
+                {yourCrib ? "Deal" : `${dealer.name} is dealing...`}
+            </Button></>} />;
 }
