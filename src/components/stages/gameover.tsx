81ca4a983243b73f19821a09250797e7e49f7f3a
macflash <alex.brofo@outlook.com>
2020-01-02 13:52:24 -0800

    add scores to game over

--- src/components/stages/gameover.tsx
+++ src/components/stages/gameover.tsx
@@ -17,6 +17,10 @@ export const ColoredSpan: React.FC<{ player?: PlayerState }> = props => {
     return <span style={{ color: props.player?.color }}>{props.children}</span>
 }
 
+export const ColoredDiv: React.FC<{ player?: PlayerState }> = props => {
+    return <div style={{ color: props.player?.color }}>{props.children}</div>
+}
+
 export const GameOver: GameComponent = props => {
     const { game } = props;
     const { players, rules } = game;
@@ -26,18 +30,19 @@ export const GameOver: GameComponent = props => {
     return <div style={{ height: "100%", width: "100%", textAlign: "center" }}>
         <img alt="CribBIGage!" src={logo} style={{ maxWidth: "70%", flex: "auto", maxHeight: "30%" }} />
         <h1>{winner?.name} won!</h1>
+        <ColoredDiv player={winner}>{winner?.name} scored {winner?.score} points</ColoredDiv>
         {players.map(player => {
             if (player == winner) { return null; }
             if (player.score < .5 * pointsToWin) {
                 // double skunk
-                return <h2 style={{ color: player.color }}>{player.name} {WasOrWere(player)} <b>DOUBLE</b> skunked!</h2>
+                return <h2 style={{ color: player.color }}>{player.name} {WasOrWere(player)} <b>DOUBLE</b> skunked  with only {player.score} points!</h2>
             }
             if (player.score < .75 * pointsToWin) {
                 // double skunk
-                return <h3 style={{ color: player.color }}>{player.name} {WasOrWere(player)} skunked!</h3>
+                return <h3 style={{ color: player.color }}>{player.name} {WasOrWere(player)} skunked with {player.score} points!</h3>
             }
 
-            return <div style={{ color: player.color }}>{player.name} lost</div>;
+            return <ColoredDiv player={player}>{player.name} lost with {player.score} points</ColoredDiv>;
         })}
         <Button
             onClick={() => {
