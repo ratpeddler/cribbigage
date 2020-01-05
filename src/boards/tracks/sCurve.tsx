77f6510c0fb76fc9278e99e4040417f475ba04e5
macflash <alex.brofo@outlook.com>
2020-01-04 18:48:45 -0800

    Update tracks

--- src/boards/tracks/sCurve.tsx
+++ src/boards/tracks/sCurve.tsx
@@ -1,48 +1,62 @@
 import React from "react";
-import { createStraightSegment, createSpacer, createTrack, create90Segment, create180Segment } from "../../components/track";
+import { createStraightSegment, createSpacer, createTrack, create90Segment, create180Segment, createContent, ColorDot, SimpleDot } from "../../components/track";
 
 const length = 70;
 const width = 50;
 const players = 3;
 const straight = createStraightSegment(length, width, players);
 const long = createStraightSegment(100, width, players);
+const backer = createContent(20, 0, props => {
+    return <ColorDot {...props} dotScale={props.dotScale * .25} color="tan" />
+});
 
 export const SCurve = createTrack("S Curve", 120, 1, 3, [
-    createStraightSegment(width, width, players, 1),
+    createStraightSegment(width * .5, width, players, 1),
+    ...backer,
     long,
+    ...backer,
     long,
-    create180Segment(length, width,players,true),
-    straight,
-    straight,
-    straight,
+    ...backer,
+    create180Segment(length + 10, width,players,true),
+    ...backer,
+    long,
+    ...backer,
+    long,
+    ...backer,
     create90Segment(length, width,players,true),
+    ...backer,
     create90Segment(length, width,players,true),
-
-    createSpacer(10),
-    straight,
-    straight,
-    straight,
-    createSpacer(10),
-    
+    ...backer,
+    long,
+    ...backer,
+    long,
+    ...backer,
+    ...backer,    
     // middle curve
     create90Segment(length, width,players,true),
+    ...backer,
     create90Segment(length, width,players),
-
-    createSpacer(10),
-    straight,
-    straight,
-    straight,
-    createSpacer(10),
+    ...backer,
+    ...backer,
+    long,
+    ...backer,
+    long,
+    ...backer,
 
     
     create90Segment(length, width,players),
+    ...backer,
     create90Segment(length, width,players),
-    straight,
-    straight,
-    straight,
-    create180Segment(length, width,players),
+    ...backer,
     long,
+    ...backer,
     long,
-
-    createStraightSegment(width, width, 1, 1),
+    ...backer,
+    create180Segment(length + 10, width,players),
+    ...backer,
+    long,
+    ...backer,
+    long,
+    ...backer,
+    createStraightSegment(width * .5, width, 1, 1),
 ], "walnut");
