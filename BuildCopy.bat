455ba4071a845190a14d217c7a92e522b33e93db
ratpeddler <peter.breyfogle@gmail.com>
2020-01-13 13:15:06 -0800

    Deployment stuff

--- BuildCopy.bat
+++ BuildCopy.bat
@@ -1,2 +1,3 @@
-xcopy C:\Users\Peter\cribbigage\build  C:\Users\Peter\OneDrive\git\CribBIGage\WebContent\build /e
+rd C:\Users\Peter\OneDrive\git\CribBIGage\WebContent\build /S /Q
+xcopy C:\Users\Peter\cribbigage\build  C:\Users\Peter\OneDrive\git\CribBIGage\WebContent\build\ /e /i
 pause
