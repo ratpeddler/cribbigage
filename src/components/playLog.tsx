1e15020059c15540c7256c6fca33a95b2bca2c48
macflash <alex.brofo@outlook.com>
2022-07-10 09:20:19 -0700

    remove some logs and bugs

--- src/components/playLog.tsx
+++ src/components/playLog.tsx
@@ -6,43 +6,46 @@ import _ from "lodash";
 import { Sound, playSound } from "../sounds/playSound";
 
 export interface ILog {
-    sound?: Sound,
-    playerName: string,
-    time: number,
-    message: string,
-    score?: IScore,
+  sound?: Sound;
+  playerName: string;
+  time: number;
+  message: string;
+  score?: IScore;
 }
 
 var oldLogs: ILog[] = [];
 
 export const ResetLogs = () => {
-    oldLogs = [];
-}
+  oldLogs = [];
+};
 
-export const PlayLog: React.FC<{ game: GameState }> = props => {
-    let logs = props.game.playLog || [];
+export const PlayLog: React.FC<{ game: GameState }> = (props) => {
+  let logs = props.game.playLog || [];
 
-    React.useEffect(() => {
-        // Check if we have "new logs" and play any sounds for them
-        if (logs.length > oldLogs.length) {
-            console.log("logs were different");
-            // do any sounds for new logs and then update old logs
-            for (let i = oldLogs.length; i < logs.length; i++) {
-                console.log("playing sound", logs[i].sound);
-                playSound(logs[i].sound, logs[i].score?.score);
-            }
+  React.useEffect(() => {
+    // Check if we have "new logs" and play any sounds for them
+    if (logs.length > oldLogs.length) {
+      console.log("logs were different");
+      // do any sounds for new logs and then update old logs
+      for (let i = oldLogs.length; i < logs.length; i++) {
+        console.log("playing sound", logs[i].sound);
+        playSound(logs[i].sound, logs[i].score?.score);
+      }
 
-            oldLogs = logs;
-        }
-        else {
-            console.log("play logs were the same, not playing any sounds");
-        }
-    }, [logs]);
+      oldLogs = logs;
+    } else {
+      console.log("play logs were the same, not playing any sounds");
+    }
+  }, [logs]);
 
-    return <Column overflow="auto" fill>
-        {logs.map((l) => <div key={l.time}>
-            {l.playerName + " " + l.message}
-            {l.score && ": " + createScoreMessage(l.score)}
-        </div>)}
+  return (
+    <Column overflow='auto' fill>
+      {logs.map((l, i) => (
+        <div key={l.time + "_" + i}>
+          {l.playerName + " " + l.message}
+          {l.score && ": " + createScoreMessage(l.score)}
+        </div>
+      ))}
     </Column>
-}
+  );
+};
--- src/components/stages/deal.tsx
+++ src/components/stages/deal.tsx
@@ -7,51 +7,65 @@ import { LocalOrMultiplayer } from "./initAndWait";
 import { addLog } from "../../App";
 import { playShuffleSound } from "../../sounds/playSound";
 
-export const Deal: GameComponent = props => {
-    //const [hasRefreshed, setRefreshed] = React.useState(false);
-    //console.log(props, hasRefreshed);
+export const Deal: GameComponent = (props) => {
+  const Layout = props.layout;
 
-    const Layout = props.layout;
+  let dealer = getCurrentDealer(props.game);
+  let yourCrib = IsYou(dealer);
 
-    let dealer = getCurrentDealer(props.game);
-    let yourCrib = IsYou(dealer);
+  React.useEffect(playShuffleSound, []);
 
-    React.useEffect(playShuffleSound, []);
+  React.useEffect(() => {
+    if (!yourCrib) {
+      if (LocalOrMultiplayer == "local") {
+        setTimeout(() => {
+          console.log("local not your crib deal");
+          addLog(props.game, dealer, "is dealer", "Deal");
+          props.setGameState(RunDeal(props.game), true);
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          // multiplayer we should refresh here.
+          props.refreshFromServer?.();
+        }, 1000);
+      }
+    }
+  }, []);
 
-    React.useEffect(() => {
-        if (!yourCrib) {
-            if (LocalOrMultiplayer == "local") {
-                setTimeout(() => {
-                    console.log("local not your crib deal");
-                    addLog(props.game, dealer, "is dealer", "Deal");
-                    props.setGameState(RunDeal(props.game), true);
-                }, 1000);
-            }
-            else {
-                setTimeout(() => {
-                    // multiplayer we should refresh here.
-                    props.refreshFromServer?.();
-                }, 1000);
-            }
-        }
-    }, []);
+  // TODO: Animate dealing
 
-    // TODO: Animate dealing
-
-    return <Layout game={props.game} setGameState={props.setGameState} userActions={() =>
+  return (
+    <Layout
+      game={props.game}
+      setGameState={props.setGameState}
+      userActions={() => (
         <>
-            {yourCrib && <h3>It's <span style={{ color: dealer.color }}>your</span> turn to deal</h3>}
-            {!yourCrib && props.refreshFromServer && <Button disabled={props.waitingForServer} onClick={props.refreshFromServer}>Next</Button>}
+          {yourCrib && (
+            <h3>
+              It's <span style={{ color: dealer.color }}>your</span> turn to
+              deal
+            </h3>
+          )}
+          {!yourCrib && props.refreshFromServer && (
             <Button
-                disabled={!yourCrib}
-                loading={!yourCrib}
-                big={true}
-                onClick={() => {
-                    console.log("you clicked deal");
-                    addLog(props.game, dealer, "is dealer", "Deal");
-                    props.setGameState(RunDeal(props.game), true);
-                }}
-            >
-                {yourCrib ? "Deal" : `${dealer.name} is dealing...`}
-            </Button></>} />;
-}
+              disabled={props.waitingForServer}
+              onClick={props.refreshFromServer}>
+              Next
+            </Button>
+          )}
+          <Button
+            disabled={!yourCrib}
+            loading={!yourCrib}
+            big={true}
+            onClick={() => {
+              console.log("you clicked deal");
+              addLog(props.game, dealer, "is dealer", "Deal");
+              props.setGameState(RunDeal(props.game), true);
+            }}>
+            {yourCrib ? "Deal" : `${dealer.name} is dealing...`}
+          </Button>
+        </>
+      )}
+    />
+  );
+};
